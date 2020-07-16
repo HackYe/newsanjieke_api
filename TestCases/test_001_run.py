571de973a19d352a65e09d3d91f1a729db959956
YuanYe <3004@sina.com>
2020-07-16 15:39:47 +0800

    主要加入异常用例的断言

--- TestCases/test_001_run.py
+++ TestCases/test_001_run.py
@@ -17,7 +17,7 @@ from Common.get_data import gd
 
 test_data = excel_data.get_excel_data()
 
-print(test_data)
+# print(test_data)
 
 
 @ddt.ddt
@@ -25,7 +25,7 @@ class TestRunMain(unittest.TestCase):
 
     @ddt.data(*test_data)
     def test_run_case(self, test_data):
-        global header
+        global header, code, msg
         global res_value
         case_id = test_data[0]
         i = excel_data.get_rows_number(case_id)
@@ -67,16 +67,47 @@ class TestRunMain(unittest.TestCase):
             #     pass
             # if is_header == 'yes':
             #     pass
+            excepect_method = test_data[10]
+            excepect_result = test_data[11]
             res = request.run_main(method, url, data, header, file)
-            code = res['code']
-            print('code是------------>', code)
-            msg = res['msg']
-            print('msg是------------->', msg)
+            try:
+                code = res['code']
+                print('code是------------>', code)
+                msg = res['msg']
+                print('msg是------------->', msg)
+            except:
+                excel_data.excel_write_data(i, 13, str(res))
+                try:
+                    message = res['message']
+                    status_code = res['status_code']
+                    if excepect_method == 'code':
+                        try:
+                            self.assertEqual(excepect_result, status_code)
+                            excel_data.excel_write_data(i, 14, 'PASS')
+                        except Exception as e:
+                            excel_data.excel_write_data(i, 14, 'FAIL')
+                            raise e
+                    elif excepect_method == 'msg':
+                        try:
+                            self.assertEqual(excepect_result, message)
+                            excel_data.excel_write_data(i, 14, 'PASS')
+                        except Exception as e:
+                            excel_data.excel_write_data(i, 14, 'FAIL')
+                            raise e
+                    elif excepect_method == 'json':
+                        try:
+                            json_res = handle_result_json(res, eval(excepect_result))
+                            self.assertTrue(json_res)
+                            excel_data.excel_write_data(i, 14, 'PASS')
+                        except Exception as e:
+                            excel_data.excel_write_data(i, 14, 'FAIL')
+                            raise e
+                except:
+                    excel_data.excel_write_data(i, 14, 'ERROR')
+
             result = str(res).encode('UTF-8')
             # 设置编码格式
             excel_data.excel_write_data(i, 13, result)
-            excepect_method = test_data[10]
-            excepect_result = test_data[11]
             if excepect_method == 'code':
                 try:
                     self.assertEqual(excepect_result, code)
